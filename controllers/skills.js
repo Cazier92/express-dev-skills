3852ba6ebc74285e42a1e35022e20216a8e57dd4
Cazier92 <bryce.cazier@gmail.com>
2023-01-05 17:42:04 -0800

    complete URL/Route Parameters, add input for bryceKnows in new.ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -19,11 +19,25 @@ function newSkill(req, res) {
 }
 
 function create(req, res) {
-  console.log(req.body)
-  req.body.bryceKnows = true
+  // console.log(req.body)
+  // req.body.bryceKnows = false
+  (req.body.bryceKnows === 'on') ? req.body.bryceKnows = true : req.body.bryceKnows = false
   Skill.create(req.body)
   .then(skill => {
     res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill
+    })
   })
   .catch(error => {
     console.log(error)
@@ -35,4 +49,5 @@ export {
   index,
   newSkill as new,
   create,
+  show,
 }
